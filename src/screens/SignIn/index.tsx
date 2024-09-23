560dba9876b3c9444ab802736255600053d0e4f1
fernandoabf <capcakeplay@hotmail.com>
2024-09-23 19:20:57 -0300

    fixed unfocus on keyboard

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,23 +1,26 @@
-import { Image, Text, View } from "react-native";
+import { Image, Keyboard, Text, TouchableWithoutFeedback, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./style";
 import { Logo } from "../../components/Logo";
 import { Login } from "../../components/Login";
 import { Button } from "../../components/Button";
 
+
 export function SignIn() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Logo />
-      <Login
-        loginTitle='Digite sua Matrícula'
-        passwordTitle='Digite sua Data de Nascimento'
-      />
-      <Button
-        title="Esqueceu sua Matrícula?"
-        type="secondary"
-        style={{ marginTop: "30%" }}
-      />
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <SafeAreaView style={styles.container}>
+        <Logo />
+        <Login
+          loginTitle='Digite sua Matrícula'
+          passwordTitle='Digite sua Data de Nascimento'
+        />
+        <Button
+          title="Esqueceu sua Matrícula?"
+          type="secondary"
+          style={{ marginTop: "30%" }}
+        />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   )
 }
